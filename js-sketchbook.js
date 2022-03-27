5542b29067f2b33417c6caa005b92463ad74ae23
2DDraghici <70889720+2DDraghici@users.noreply.github.com>
2022-03-27 15:51:11 +0300

    Add files via upload

--- js-sketchbook.js
+++ js-sketchbook.js
@@ -1,18 +1,52 @@
+let color = 'black'
+let body= document.querySelector('body')
+body.addEventListener("mouseup", mouseUp)
+body.addEventListener("mousedown", mouseDown)
 
-let index;
-let gridSize = 16;
-const canvas = document.querySelector('#canvas')
+function populateBoard(size) {
+    let grid = document.querySelector('.grid');
+    let squares = grid.querySelectorAll("div")
+    squares.forEach(div => div.remove())
+    grid.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+    let dimension = size * size;
+    for (let i = 0; i < dimension; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square")
+        square.addEventListener("mouseover", colorSquare)
+        
 
-const btn = document.querySelector('#btn')
 
-function createGrid(number)
-{  
-    for (index = 0; index < number*number; index++) {
-        const pixel2 =  document.createElement('div')
-        pixel2.classList.add('pixel')
-        canvas.appendChild(pixel2);
-        
-        
-        
+        grid.insertAdjacentElement("beforeend", square);
     }
+}
+
+let checkMouse = false
+function mouseDown() {
+    checkMouse = true;
+}
+function mouseUp() {
+    checkMouse = false;
+}
+populateBoard(16);
+function changeSize(input) {
+    populateBoard(input)
+}
+function colorSquare() {
+    if (checkMouse) {
+        this.style.backgroundColor = color;
+    }
+}
+function changeColor(choice) {
+    color = choice
+
+}
+function Eraser() {
+    color = "white"
+}
+
+function resetGrid() {
+    let grid = document.querySelector('.grid');
+    let squares = grid.querySelectorAll("div")
+    squares.forEach(div => div.style.backgroundColor = "white")
 }
